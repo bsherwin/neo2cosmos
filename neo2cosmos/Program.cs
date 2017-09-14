c9a2a175a7f847dca7ada3d9feef0eee4c77df7a
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2017-09-14 16:46:03 +0200

    Fix migration errors

--- neo2cosmos/Program.cs
+++ neo2cosmos/Program.cs
@@ -4,62 +4,84 @@ using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
-using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.Graphs;
 using Neo4j.Driver.V1;
 using System.Diagnostics;
+using System.Linq;
+using System.Net;
 
 namespace neo2cosmos
 {
     public class Program
     {
-        private static string CosmosEndpoint;
-        private static string CosmosAuthKey;
-        private static string NeoBolt;
-        private static string NeoUser;
-        private static string NeoPass;
+        private const string DatabaseId = "graphdb";
+        private const string CollectionId = "Northwind";
+
+        private static string _cosmosEndpoint;
+        private static string _cosmosAuthKey;
+
+        private static string _neoBolt;
+        private static string _neoUser;
+        private static string _neoPass;
 
         public static void Main(string[] args)
         {
-            CosmosEndpoint = ConfigurationManager.AppSettings["CosmosEndpoint"];
-            CosmosAuthKey = ConfigurationManager.AppSettings["CosmosAuthKey"];
-            NeoBolt = ConfigurationManager.AppSettings["NeoBolt"];
-            NeoUser = ConfigurationManager.AppSettings["NeoUser"];
-            NeoPass = ConfigurationManager.AppSettings["NeoPass"];
+            LoadConfiguration();
 
-            using (DocumentClient client = new DocumentClient(
-                 new Uri(CosmosEndpoint),
-                 CosmosAuthKey,
-                 new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp }))
+            using (var client = new DocumentClient(
+                new Uri(_cosmosEndpoint),
+                _cosmosAuthKey,
+                new ConnectionPolicy {ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp}))
             {
-                Program p = new Program();
+                var p = new Program();
                 p.RunAsync(client).Wait();
             }
         }
 
+        private static void LoadConfiguration()
+        {
+            _cosmosEndpoint = ConfigurationManager.AppSettings["CosmosEndpoint"];
+            _cosmosAuthKey = ConfigurationManager.AppSettings["CosmosAuthKey"];
+
+            _neoBolt = ConfigurationManager.AppSettings["NeoBolt"];
+            _neoUser = ConfigurationManager.AppSettings["NeoUser"];
+            _neoPass = ConfigurationManager.AppSettings["NeoPass"];
+        }
+
         public async Task RunAsync(DocumentClient client)
         {
-            Database database = await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "graphdb" });
-            await client.DeleteDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri("graphdb", "Northwind"));
-            DocumentCollection graph = await client.CreateDocumentCollectionIfNotExistsAsync(
-                UriFactory.CreateDatabaseUri("graphdb"),
-                new DocumentCollection { Id = "Northwind" },
-                new RequestOptions { OfferThroughput = 400 });
+            await client.CreateDatabaseIfNotExistsAsync(new Database {Id = DatabaseId});
+
+            try
+            {
+                await client.DeleteDocumentCollectionAsync(
+                    UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode != HttpStatusCode.NotFound)
+                    throw;
+            }
+
+            var graph = await client.CreateDocumentCollectionIfNotExistsAsync(
+                UriFactory.CreateDatabaseUri(DatabaseId),
+                new DocumentCollection {Id = CollectionId},
+                new RequestOptions {OfferThroughput = 400});
 
             await CreateVertexes(client, graph);
             await CreateEdges(client, graph);
         }
 
-        private async Task CreateVertexes(DocumentClient client, DocumentCollection graph)
+        private static async Task CreateVertexes(DocumentClient client, DocumentCollection graph)
         {
-            var tasks = new List<Task<FeedResponse<dynamic>>>();
             var s = new Stopwatch();
             s.Start();
             await Task.Run(() =>
-                Parallel.ForEach(ReadNeoVertexes(), new ParallelOptions { MaxDegreeOfParallelism = 4 }, item => {
-                    string gremlinVertex = ConvertItemToGremlinVertex(item);
+                Parallel.ForEach(ReadNeoVertexes(), new ParallelOptions {MaxDegreeOfParallelism = 4}, item =>
+                {
+                    var gremlinVertex = ConvertItemToGremlinVertex(item);
                     Console.WriteLine(gremlinVertex);
-                    IDocumentQuery<dynamic> query = client.CreateGremlinQuery<dynamic>(graph, gremlinVertex);
+                    var query = client.CreateGremlinQuery<dynamic>(graph, gremlinVertex);
                     while (query.HasMoreResults)
                     {
                         var sw = new Stopwatch();
@@ -73,16 +95,16 @@ namespace neo2cosmos
             Console.WriteLine($"Total Time: {s.ElapsedMilliseconds} ms");
         }
 
-        private async Task CreateEdges(DocumentClient client, DocumentCollection graph)
+        private static async Task CreateEdges(DocumentClient client, DocumentCollection graph)
         {
-            var tasks = new List<Task<FeedResponse<dynamic>>>();
             var s = new Stopwatch();
             s.Start();
             await Task.Run(() =>
-                Parallel.ForEach(ReadNeoEdges(), new ParallelOptions { MaxDegreeOfParallelism = 4 }, item => {
-                    string gremlinEdge = ConvertItemToGremlinEdge(item);
+                Parallel.ForEach(ReadNeoEdges(), new ParallelOptions {MaxDegreeOfParallelism = 4}, item =>
+                {
+                    var gremlinEdge = ConvertItemToGremlinEdge(item);
                     Console.WriteLine(gremlinEdge);
-                    IDocumentQuery<dynamic> query = client.CreateGremlinQuery<dynamic>(graph, gremlinEdge);
+                    var query = client.CreateGremlinQuery<dynamic>(graph, gremlinEdge);
                     while (query.HasMoreResults)
                     {
                         var sw = new Stopwatch();
@@ -94,70 +116,63 @@ namespace neo2cosmos
                 })
             );
             Console.WriteLine($"Total Time: {s.ElapsedMilliseconds} ms");
-
         }
 
-        private List<INode> ReadNeoVertexes()
+        private static IDriver CreateNeo4JDriver()
+        {
+            var noSsl = new Config { EncryptionLevel = EncryptionLevel.None };
+            return GraphDatabase.Driver(_neoBolt, AuthTokens.Basic(_neoUser, _neoPass), noSsl);
+        }
+
+        private static IEnumerable<INode> ReadNeoVertexes()
         {
             var returnResult = new List<INode>();
-            Config noSSL = new Config();
-            noSSL.EncryptionLevel = EncryptionLevel.None;
 
-            using (var driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "gremlin"), noSSL))
+            using (var driver = CreateNeo4JDriver())
             using (var session = driver.Session())
             {
                 var result = session.Run("MATCH (n) RETURN n");
-                foreach (var record in result)
-                {
-                    returnResult.Add(record["n"].As<INode>());
-                }
+                returnResult.AddRange(result.Select(record => record["n"].As<INode>()));
             }
             return returnResult;
         }
 
-        private List<IRelationship> ReadNeoEdges()
+        private static IEnumerable<IRelationship> ReadNeoEdges()
         {
             var returnResult = new List<IRelationship>();
-            Config noSSL = new Config();
-            noSSL.EncryptionLevel = EncryptionLevel.None;
 
-            using (var driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "gremlin"), noSSL))
+            using (var driver = CreateNeo4JDriver())
             using (var session = driver.Session())
             {
                 var result = session.Run("MATCH (a)-[r]->(b) RETURN r");
-                foreach (var record in result)
-                {
-                    returnResult.Add(record["r"].As<IRelationship>());
-                }
+                returnResult.AddRange(result.Select(record => record["r"].As<IRelationship>()));
             }
             return returnResult;
         }
 
-        private string ConvertItemToGremlinVertex(INode item)
+        private static string ConvertItemToGremlinVertex(INode item)
         {
-            string vertex = string.Format("g.addV('{0}')", item.Labels[0]);
-            vertex += string.Format(".property('id', '{0}')", item.Id);
+            var vertex = $"g.addV('{item.Labels[0]}')";
+            vertex += $".property('id', '{item.Id}')";
             foreach (var prop in item.Properties)
             {
-                string propValue = prop.Value.ToString().Replace("'", "");
-                vertex += string.Format(".property('{0}', '{1}')", prop.Key, propValue);
+                var propValue = prop.Value.ToString().Replace("'", "");
+                vertex += $".property('{prop.Key}', '{propValue}')";
             }
             return vertex;
         }
 
-        private string ConvertItemToGremlinEdge(IRelationship item)
+        private static string ConvertItemToGremlinEdge(IRelationship item)
         {
-            string edge = string.Format("g.V('{0}')", item.StartNodeId);
-            edge += string.Format(".addE('{0}')", item.Type);
+            var edge = $"g.V('{item.StartNodeId}')";
+            edge += $".addE('{item.Type}')";
             foreach (var prop in item.Properties)
             {
-                string propValue = prop.Value.ToString().Replace("'", "");
-                edge += string.Format(".property('{0}', '{1}')", prop.Key, propValue);
+                var propValue = prop.Value.ToString().Replace("'", "");
+                edge += $".property('{prop.Key}', '{propValue}')";
             }
-            edge += string.Format(".to(g.V('{0}'))", item.EndNodeId);
+            edge += $".to(g.V('{item.EndNodeId}'))";
             return edge;
         }
-
     }
-
 }
